c7cc1702053d2c3e0413060dd25742fc666219bc
Md. Mostak Shahid <mostak.shahid@gmail.com>
2023-02-17 11:04:46 +0600

    1.0.11

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header() ?>
 <?php if (!is_front_page()) :?>
-    <section class="page-title py-5 bg-theme text-white">
+    <section class="page-title">
         <div class="wrapper">
             <div class="container">
                 <h1><?php echo get_the_title() ?></h1>
